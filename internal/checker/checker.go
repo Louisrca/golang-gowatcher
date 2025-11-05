21039656d4ef90f18e0713348823e27c8e7f2fdf
LouisR17 <roccalouis26@gmail.com>
2025-11-05 14:22:03 +0100

    checker up

---
+++ internal/checker/checker.go
@@ -0,0 +1,34 @@
+package checker
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type CheckResult struct {
+	Target string
+	Status string
+	Err    error
+}
+
+func CheckURL(url string, results chan<- CheckResult) {
+	// Timeout court pour éviter de bloquer trop
+	client := http.Client{
+		Timeout: 3 * time.Second,
+	}
+	resp, err := client.Get(url)
+	if err != nil {
+		results <- CheckResult{
+			Target: url,
+			Err:    fmt.Errorf("Request failed: %w", err),
+		}
+		return
+	}
+	defer resp.Body.Close()
+
+	results <- CheckResult{
+		Target: url,
+		Status: resp.Status,
+	}
+}
